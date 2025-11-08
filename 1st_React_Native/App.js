c0fd273effcf33ac976a8b6c4d023a2079ff8079
Daffyl Fran <1700261@ub.edu.ph>
2025-11-08 23:36:53 +0800

    added a moveable blue box

--- 1st_React_Native/App.js
+++ 1st_React_Native/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {use, useRef} from "react";
 import {
   StyleSheet,
   View,
@@ -6,12 +6,30 @@ import {
   Image,
   Alert,
   Platform,
-  TouchableOpacity
+  TouchableOpacity,
+  Animated,
+  PanResponder,
 } from "react-native";
+
 const onPressButton = () =>{
   Alert.alert("Button Pressed!");
 };
+
 const App = () => {
+ const pan = useRef(new Animated.ValueXY()).current;
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, {dx: pan.x, dy: pan.y}]),
+      onPanResponderRelease: () => {
+        Animated.spring(pan, {
+          toValue: {x: 0, y: 0},
+          useNativeDriver: true,
+        }).start();
+      },
+    }),
+  ).current;
+
   return (
     <View style={[styles.container, {flex: 1}]}>
       <View style={styles.centerArea}>
@@ -33,6 +51,16 @@ const App = () => {
         </View>
       </TouchableOpacity>
 
+           <View style={styles.boxContainer}>
+      <Animated.View
+        style={{
+          transform: [{translateX: pan.x}, {translateY: pan.y}],
+        }}
+        {...panResponder.panHandlers}>
+        <View style={styles.moveableBox}/>
+      </Animated.View>
+    </View>
+
       </View>
     </View>
   );
@@ -43,6 +71,18 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 50,
     backgroundColor: "powderblue"
+  },
+  moveableBox: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'blue',
+    borderRadius: 5,
+  },
+  boxContainer: {
+    marginTop: 100,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   button: {
     marginTop: 15,
